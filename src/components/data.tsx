35f7204a5b4ba8307c1d1f3bdcc8dc091011d5d0
AsumVictor <iamasum369@gmail.com>
2024-02-21 12:53:23 +0000

    Adde hero page with placeholders

---
+++ src/components/data.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Problem_soln_pitch() {
+  return (
+    <span>
+      Forget the dream of sustainable coastlines; we're making them a reality! We're engineering bacteria to build the future! This project is creating
+      self-repairing, bioluminescent bioconcrete tetrapods to combat <del className=" bg-red-300 px-2 font-bold mr-1">
+      Plastic pollution </del> and <strong>Coastal erosion.</strong> 
+    </span>
+  );
+}
--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import Button from "../topology/Button";
+import { Problem_soln_pitch } from "../data";
 
 function HeroSection(): JSX.Element {
   return (
-    <section className="w-full hero-bg 1240px:h-[90vh] py-20 1240px:py-0 1240px:mt-[-1.8cm] flex justify-center items-center overflow-x-hidden">
+    <section className="w-full hero-bg 1240px:h-[100vh] py-20 1240px:py-0 1240px:mt-[-1.8cm] flex justify-center items-center overflow-x-hidden">
       <div className="w-full flex gap-10 flex-col 1240px:flex-row justify-center px-5 400px:px-10 550px:px-20">
-        <div className="w-full h-[10cm] 650px:w-[10cm] bg-deep-blue"></div>
         <div className=" w-full 650px:w-[15cm] ">
-          <h1 className="text-[3rem] 400px:text-[3.5rem] 550px:text-[4rem] 650px:text-[5.5rem] protest-strike-regular text-deep-blue">
-            VICTOR ASUM
+          <h1 className="text-[3rem] 400px:text-[3.5rem] 550px:text-[4rem] 650px:text-[5rem] protest-strike-regular text-deep-blue leading-[5rem]">
+          CoastBusters, <br /> To the Rescue!
           </h1>
-          <p className=" font-semibold poppins-regular">{"hhh"}</p>
+          <p className=" font-semibold poppins-regular"><Problem_soln_pitch /></p>
           <div className="mt-7">
             <Button
               type="button"
               title="let talk"
               size="medium"
-              textLabel="Let's Talk"
+              textLabel="Learn more here"
             />
           </div>
         </div>
+        <div className="w-full h-[10cm] 650px:w-[10cm] bg-deep-blue"></div>
       </div>
     </section>
   );
