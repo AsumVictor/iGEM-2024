9b592283d1c6261ee6ecaed8677d3d1a75ae78b0
AsumVictor <iamasum369@gmail.com>
2024-02-22 09:42:27 +0000

    Improved SEO search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "iGEMxAshesi2024",
   description: "Website for iGEM application proccess",
+  applicationName: "Asum Victor",
+  icons: ["/icon.png"],
+  appleWebApp: true,
   authors: [
     {
       url: "https://www.vrasum.tech",
